1dd4aed6114afc9b0f78370eb1730ff72dcd2a2f
robedgar <116982166+robedgar@users.noreply.github.com>
2023-01-17 19:48:42 +0000

    add multi-wallet login

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,4 +1,5 @@
 import { useMetamask } from '@thirdweb-dev/react';
+import { ConnectWallet } from '@thirdweb-dev/react';
 import React from 'react';
 
 function Login() {
@@ -14,15 +15,22 @@ function Login() {
         />
         <h1 className="text-6xl text-white font-bold pb-2">The Crypto Lotto</h1>
         <h2 className="text-white ">
-          Get Started By logging in with your MetaMask
+          Get Started By logging in with your Wallet
         </h2>
 
-        <button
-          onClick={connectWithMetamask}
-          className="bg-white px-8 py-5 mt-10 rounded-lg shadow-lg font-bold"
-        >
-          Login with MetaMask
-        </button>
+        <div className=" pt-4">
+          <ConnectWallet
+            // Some customization of the button style
+            colorMode="dark"
+            accentColor="white"
+          />
+        </div>
+        <div>
+          <p className="text-white pt-4 italic">
+            Polygon combines the best of Ethereum and sovereign blockchains into
+            a full-fledged multi-chain system.
+          </p>
+        </div>
       </div>
     </div>
   );
